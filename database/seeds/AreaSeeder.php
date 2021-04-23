80ae367a89ecf3a1900a3b9b57249cc900838bfc
jfgils02 <jfgils02@gmail.com>
2021-04-23 13:23:27 -0500

    cambios en folders

--- database/seeds/AreaSeeder.php
+++ database/seeds/AreaSeeder.php
@@ -15,7 +15,7 @@ class AreaSeeder extends Seeder
         $data = [
             ['name' => 'Almacen', 'created_at' => date('Y-m-d H:m:s'), 'updated_at' => date('Y-m-d H:m:s')],
             ['name' => 'Calidad', 'created_at' => date('Y-m-d H:m:s'), 'updated_at' => date('Y-m-d H:m:s')],
-            ['name' => 'Control Operacional', 'created_at' => date('Y-m-d H:m:s'), 'updated_at' => date('Y-m-d H:m:s')],
+            ['name' => 'Operaciones', 'created_at' => date('Y-m-d H:m:s'), 'updated_at' => date('Y-m-d H:m:s')],
             ['name' => 'Compras', 'created_at' => date('Y-m-d H:m:s'), 'updated_at' => date('Y-m-d H:m:s')],
             ['name' => 'Direccion', 'created_at' => date('Y-m-d H:m:s'), 'updated_at' => date('Y-m-d H:m:s')],
             ['name' => 'Finanzas', 'created_at' => date('Y-m-d H:m:s'), 'updated_at' => date('Y-m-d H:m:s')],
