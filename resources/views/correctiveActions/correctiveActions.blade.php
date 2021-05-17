03504b6d96a2d7fff1c6602daee9919315e0be7e
jfgils02 <jfgils02@gmail.com>
2021-05-17 11:35:31 -0500

    cambios

--- resources/views/correctiveActions/correctiveActions.blade.php
+++ resources/views/correctiveActions/correctiveActions.blade.php
@@ -88,7 +88,7 @@
                                     <th>Archivos</th>
                                     <th>#</th>
                                     <th>Problematica</th>
-                                    <th>Acción inmediata</th>
+                                    <th>Tipo de acción</th>
                                     <th>Responsable</th>
                                     <th>Involucrados</th>
                                     <th>Estatus</th>
