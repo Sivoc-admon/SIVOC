e22a128126edc4d1c448fefd94261b278b4ef7b5
jfgils02 <jfgils02@gmail.com>
2021-05-24 11:50:12 -0500

    cambios

--- routes/web.php
+++ routes/web.php
@@ -122,34 +122,3 @@ Route::resource('rh', 'RhController');
 
 
 
-
-/*Auth::routes();
- 
-Route::get('/home', 'HomeController@index')->name('home');
- 
-//Roles y Permisos
- 
-Route::middleware(['auth'])->group(function(){
- 
-	//Products
-	Route::post('products/store', 'ProductController@store')->name('products.store')
-		->middleware('permission:products.create');
- 
-	Route::get('products', 'ProductController@index')->name('products.index')
-		->middleware('permission:products.index');
- 
-	Route::get('products/create', 'ProductController@create')->name('products.create')
-		->middleware('permission:products.create');
- 
-	Route::put('products/{product}', 'ProductController@update')->name('products.update')
-		->middleware('permission:products.edit');
- 
-	Route::get('products/{product}', 'ProductController@show')->name('products.show')
-		->middleware('permission:products.show');
- 
-	Route::delete('products/{product}', 'ProductController@destroy')->name('products.destroy')
-		->middleware('permission:products.destroy');
- 
-	Route::get('products/{product}/edit', 'ProductController@edit')->name('products.edit')
-		->middleware('permission:products.edit'); */
-
