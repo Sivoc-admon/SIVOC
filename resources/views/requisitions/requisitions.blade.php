218f80f81fccfb9624521b9f404619fb505e4fb2
Francisco <jfgils02@gmail.com>
2023-04-18 14:56:33 -0600

    cambios

--- resources/views/requisitions/requisitions.blade.php
+++ resources/views/requisitions/requisitions.blade.php
@@ -115,7 +115,7 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
-                    @if(Auth::user()->hasAnyRole(['admin', 'almacen', 'finanzas ', 'calidad', 'compras', 'tesoreria', 'rh', 'manufactura', 'pruebas', 'ingenieria', 'ventas', 'direccion', 'coordinador comercial', 'coordinador operacional', 'coordinador de finanzas', 'coordinador de calidad']))
+                    @if(Auth::user()->hasAnyRole(['admin', 'almacen', 'finanzas ', 'calidad', 'compras', 'finanzas', 'rh', 'manufactura', 'pruebas', 'ingenieria', 'ventas', 'direccion', 'coordinador comercial', 'coordinador operacional', 'coordinador de finanzas', 'coordinador de calidad']))
 
                     <span data-toggle="modal" data-target="#modalCreateRequisition" data-backdrop='static'>
                         <button type="button" class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="Nueva Requisición" onclick="newRequisition()">
@@ -184,7 +184,7 @@
                                         <td>{{ $requisiton->comment }}</td>
 
 
-                                        @if (Auth::user()->hasAnyRole(['admin', 'almacen', 'calidad', 'compras', 'manufactura', 'pruebas', 'ingenieria', 'rh', 'ventas', 'tesoreria', 'direccion', 'finanzas', 'coordinador comercial', 'coordinador operacional', 'coordinador finanzas', 'coordinador de calidad']))
+                                        @if (Auth::user()->hasAnyRole(['admin', 'almacen', 'calidad', 'compras', 'manufactura', 'pruebas', 'ingenieria', 'rh', 'ventas', 'finanzas', 'direccion', 'finanzas', 'coordinador comercial', 'coordinador operacional', 'coordinador finanzas', 'coordinador de calidad']))
                                             <td>
                                                 <span data-toggle="modal" data-target="#modalCreateRequisition" data-backdrop='static'>
                                                     <button type="button" class="btn btn-success" data-toggle="tooltip" data-placement="top" title="Editar requisicion" onclick="showRequisition({{$requisiton->id}})">
@@ -201,7 +201,7 @@
                                                         <i class="fas fa-clock"></i>
                                                     </button>
                                                 </span>
-                                                @if (Auth::user()->hasAnyRole(['admin', 'direccion', 'almacen', 'calidad', 'finanzas', 'pruebas', 'ingenieria', 'ventas', 'rh', 'manufactura', 'coordinador comercial', 'coordinador operacional', 'coordinador finanzas', 'coordinador de calidad', 'compras']))
+                                                @if (Auth::user()->hasAnyRole(['admin', 'direccion', 'almacen', 'calidad', 'finanzas', 'pruebas', 'ingenieria', 'ventas', 'rh', 'finanzas', 'manufactura', 'coordinador comercial', 'coordinador operacional', 'coordinador finanzas', 'coordinador de calidad', 'compras']))
                                                     @switch($requisiton->status)
                                                         @case("Creada")
                                                                 @switch($requisiton->id_area)
